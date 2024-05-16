1c3d69efdf62499e65091be8f9b66427f74759a4
Lucas Climenhage <luke.climenhage@gmail.com>
2024-05-15 21:17:19 -0400

    Update social links

--- data/socials.ts
+++ data/socials.ts
@@ -6,30 +6,15 @@ type Social = {
 
 const socials: Social[] = [
   {
-    name: 'Instagram',
-    logo: 'SvgoInstagram',
-    link: 'https://www.instagram.com/hugo.rcd_',
+    name: "GitHub",
+    logo: "SvgoGithub",
+    link: "https://github.com/lukeclimen",
   },
   {
-    name: 'GitHub',
-    logo: 'SvgoGithub',
-    link: 'https://github.com/HugoRCD',
+    name: "LinkedIn",
+    logo: "SvgoLinkedin",
+    link: "https://www.linkedin.com/in/lclimenh/",
   },
-  {
-    name: 'X / Twitter',
-    logo: 'SvgoX',
-    link: 'https://twitter.com/HugoRCD__',
-  },
-  {
-    name: 'LinkedIn',
-    logo: 'SvgoLinkedin',
-    link: 'https://www.linkedin.com/in/hugo-richard-0801/',
-  },
-  {
-    name: 'Spotify',
-    logo: 'SvgoSpotify',
-    link: 'https://open.spotify.com/user/yuvl0zpp3bpx4hne1ag7huten?si=df7ee2777c0c4fc4',
-  },
-]
+];
 
-export default socials
+export default socials;
